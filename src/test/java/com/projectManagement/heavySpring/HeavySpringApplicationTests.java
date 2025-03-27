35809c71d6181a8f4d00fa54d04ef43c6663b7a2
DRTX2 <davidmanjarres2004@gmail.com>
2025-03-26 19:18:16 -0500

    First commit

---
+++ src/test/java/com/projectManagement/heavySpring/HeavySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectManagement.heavySpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeavySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
