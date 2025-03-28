43fbf2c85f1d7ad040a9dbb0e6908979e5f2122e
DRTX2 <davidmanjarres2004@gmail.com>
2025-03-27 23:54:41 -0500

    Fix pakcages configs

--- src/main/java/com/projectManagement/heavySpring/app/HeavySpringApplication.java
+++ src/main/java/com/projectManagement/heavySpring/app/HeavySpringApplication.java
@@ -1,13 +1,16 @@
-package com.projectManagement.heavySpring;
+package com.projectManagement.heavySpring.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+		"com.projectManagement.heavySpring.app",
+		"com.projectManagement.heavySpring.adapters",
+		"com.projectManagement.heavySpring.core"
+})
 @EnableJpaRepositories(basePackages = "com.projectManagement.heavySpring.adapters.persistence")
-// Habilita el uso de repositorios JPA dentro del paquete especificado y permite que Spring cree automáticamente las implementaciones de esos repositorios.
 @EntityScan(basePackages = "com.projectManagement.heavySpring.adapters.persistence")
 // Le indica a Spring dónde encontrar las clases @Entity que representan las tablas de la base de datos.
 public class HeavySpringApplication {
